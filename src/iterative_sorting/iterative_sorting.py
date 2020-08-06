9f876138b5d4c22b8cf09dc6c28a81ef056cbdaa
mpHarm88 <mpHarm88@users.noreply.github.com>
2020-08-05 18:58:28 -0700

    add sort mod 1

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -4,22 +4,40 @@ def selection_sort( arr ):
     for i in range(0, len(arr) - 1):
         cur_index = i
         smallest_index = cur_index
+
         # TO-DO: find next smallest element
-        # (hint, can do in 3 loc) 
+        # (hint, can do in 3 loc)
+        for j in range(cur_index, len(arr)):
+            if arr[smallest_index] > arr[j]:
+                smallest_index = j
              
-
-
-
         # TO-DO: swap
-
-
-
+        temp = arr[cur_index]
+        arr[cur_index] = arr[smallest_index]
+        arr[smallest_index] = temp
 
     return arr
 
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
+
+    for _ in range(0, len(arr)-1):
+        cur_index = 0
+    
+        while cur_index < len(arr)-1: 
+            # if the previous number is bigger than the next number then swap
+            if arr[cur_index] > arr[cur_index+1]:
+                
+                # Swap
+                temp = arr[cur_index]
+                arr[cur_index] = arr[cur_index+1]
+                arr[cur_index+1] = temp
+                
+                ## Increment cur_index by 1
+                cur_index+=1
+            else:
+                cur_index+=1
 
     return arr
 
