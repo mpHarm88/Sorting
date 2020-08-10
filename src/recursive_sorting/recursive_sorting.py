3fb1482b056d5b4eecbd97f2478c5364ff576a93
mpHarm88 <mpHarm88@users.noreply.github.com>
2020-08-10 16:42:07 -0700

    add recursive sort

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,18 +1,37 @@
 # TO-DO: complete the helpe function below to merge 2 sorted arrays
 def merge( arrA, arrB ):
-    elements = len( arrA ) + len( arrB )
-    merged_arr = [0] * elements
-    # TO-DO
+    merged_arr = []
+    
+    while (len(arrA) > 0) & (len(arrB) > 0):
+        if arrA[0] > arrB[0]:
+            merged_arr.append(arrB[0])
+            arrB.pop(0)
+        else:
+            merged_arr.append(arrA[0])
+            arrA.pop(0)  
+
+    # Add remaining elements to the merged array
+    merged_arr.extend(arrA[:])
+    merged_arr.extend(arrB[:])
     
     return merged_arr
 
-
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort( arr ):
-    # TO-DO
+    
+    if len(arr) <= 1:
+        return arr
+    
+    middle_index = len(arr)//2
 
-    return arr
+    arrA = arr[:middle_index]
+    arrB = arr[middle_index:]
 
+    # Define sorted values
+    arrA = merge_sort(arrA)
+    arrB = merge_sort(arrB)
+
+    return merge(arrA, arrB)
 
 # STRETCH: implement an in-place merge sort algorithm
 def merge_in_place(arr, start, mid, end):
---
+++ src/recursive_sorting/sort.py
@@ -0,0 +1,91 @@
+from book import Book
+import time
+import csv
+
+def insertion_sort(books):
+    # loop through len-1 elements
+    for i in range(1, len(books)):
+        temp = books[i]
+        j = i
+        while j > 0 and temp.genre < books[j - 1].genre:
+            # shift left until correct genre found
+            books[j] = books[j - 1]
+            j -= 1
+        # insert at correct position
+        books[j] = temp
+
+    return books
+
+# Version A: Conventional, recursive Quick Sort
+def quick_sort_A( books, low, high ):
+    # TO-DO: implement Quick Sort
+
+    return books
+
+
+# Version B:
+# NOT done in place because for large inputs, we
+# exceed Python's maximum recursion depth with 
+# in-place Quick Sort
+def quick_sort_B( books ):
+    # STRETCH: implement Quick Sort for larger datasets
+
+    return books
+
+
+def book_sort(books):
+    # STRETCH: combine Insertion & Quick Sort for
+    # an improved sorting algorithm
+    
+    return books
+
+# Read in book data from CSV file
+# provided by https://github.com/uchidalab/book-dataset
+with open('book_data.csv') as csvfile:
+    my_books_long = []
+    data = csv.DictReader(csvfile)
+    for book in data:
+        #print(book['title'], book['author'], book['genre'])
+        my_books_long.append(Book(book['title'], book['author'], book['genre']))
+    my_books_medium = my_books_long[0:997]
+    my_books_short = my_books_long[0:10]
+
+print("***********~Test with 10 Books~**********")
+start = time.time()
+sorted_books = insertion_sort(my_books_short)
+end = time.time()
+print("Insertion Sort took:  " + str((end - start)*1000) + " milliseconds")
+
+start = time.time()
+sorted_books = quick_sort_A(my_books_short, 0, len(my_books_short))
+end = time.time()
+print("Quick Sort (A) took:  " + str((end - start)*1000) + " milliseconds")
+
+
+print("\n***********~Test with ~1,000 Books~**********")
+start = time.time()
+sorted_books = insertion_sort(my_books_medium)
+end = time.time()
+print("Insertion Sort took:  " + str((end - start)*1000) + " milliseconds")
+
+start = time.time()
+sorted_books = quick_sort_A(my_books_medium, 0, len(my_books_medium))
+end = time.time()
+print("Quick Sort (A) took:  " + str((end - start)*1000) + " milliseconds")
+
+
+print("\n**********~Test with +2,000 Books~**********")
+start = time.time()
+sorted_books = insertion_sort(my_books_long)
+end = time.time()
+print("Insertion Sort took:  " + str((end - start)*1000) + " milliseconds")
+
+# start = time.time()
+# sorted_books = quick_sort_B(my_books_long)
+# end = time.time()
+# print("Quick Sort (B) took:  " + str((end - start)*1000) + " milliseconds")
+
+# start = time.time()
+# sorted_books = book_sort(my_books_long)
+# end = time.time()
+# print("Book Sort took:       " + str((end - start)*1000) + " milliseconds\n")
